7cd1d38e4e5b4b2a153f9f0f8b08f134d6a9d359
flnzba <florian@fzeba.com>
2026-06-18 17:07:07 +0200

    changed ports to default

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="mailexport",
     # Change port here if 3000 is already in use
-    frontend_port=3002,
-    backend_port=8007,
+    frontend_port=3000,
+    backend_port=8000,
     plugins=[
         # Theme moved here from App(theme=...), which is deprecated in 0.9.x.
         rx.plugins.RadixThemesPlugin(
